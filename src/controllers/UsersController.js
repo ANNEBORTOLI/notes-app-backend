f6072a4880c8f26835a93cf474ef5ed0e1f96323
Anne Bortoli <annebortoli@gmail.com>
2024-08-26 17:50:49 -0300

    added users to db

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,27 @@
 const AppError = require("../utils/AppError");
 
-class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body;
+const sqliteConnection = require("../database/sqlite");
 
-    if (!name) {
-      throw new AppError("O nome é obrigatório!");
+class UsersController {
+  async create(req, res) {
+    const { name, email, password } = req.body;
+
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    response.status(201).json({ name, email, password });
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+
+    return res.status(201).json({});
   }
 }
 
