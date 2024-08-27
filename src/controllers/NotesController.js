8b5e98da8c90965abef022b062ad8c987dae6189
Anne Bortoli <annebortoli@gmail.com>
2024-08-27 15:15:57 -0300

    create notesController

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,73 @@
+const knex = require("../database/knex");
+
+class NotesController {
+  async create(req, res) {
+    const { title, description, tags, links } = req.body;
+    const { user_id } = req.params;
+
+    // Cadastra a nota no bd e recupera o id da nota que acabamos de criar
+    // O [] são necessários, pois quando usamos o insert ele retorna um array
+    //com o note_id dentro na primeira posição
+    const [note_id] = await knex("notes").insert({
+      title,
+      description,
+      user_id
+    });
+
+    // Percorre a array de links e retorna um obj representando a estrutura
+    // necessária para guardar na tabela de Links
+    const linksInsert = links.map((link) => {
+      return {
+        note_id,
+        url: link
+      };
+    });
+
+    await knex("links").insert(linksInsert);
+
+    const tagsInsert = tags.map((name) => {
+      return {
+        note_id,
+        name,
+        user_id
+      };
+    });
+
+    await knex("tags").insert(tagsInsert);
+
+    res.json();
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const note = await knex("notes").where({ id }).first();
+    const tags = await knex("tags").where({ note_id: id }).orderBy("name");
+    const links = await knex("links")
+      .where({ note_id: id })
+      .orderBy("created_at");
+
+    return res.json({
+      ...note,
+      tags,
+      links
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return res.json();
+  }
+
+  async index(req, res) {
+    const { user_id } = req.query;
+    const notes = await knex("notes").where({ user_id }).orderBy("title");
+
+    return res.json(notes);
+  }
+}
+
+module.exports = NotesController;
